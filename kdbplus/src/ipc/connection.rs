bc385b1f87711004dfb95ba7da45669edb41dacf
Jake Mitchell <11303841+0-jake-0@users.noreply.github.com>
2026-02-15 09:29:27 +0000

    remove println on connect
    Signed-off-by: Jake Mitchell <11303841+0-jake-0@users.noreply.github.com>

--- kdbplus/src/ipc/connection.rs
+++ kdbplus/src/ipc/connection.rs
@@ -889,7 +889,7 @@ async fn connect_tcp_impl(host: &str, port: u16) -> Result<TcpStream> {
         // Return if this IP address is valid
         match TcpStream::connect(&host_port).await {
             Ok(socket) => {
-                println!("connected: {}", host_port);
+                //println!("connected: {}", host_port);
                 return Ok(socket);
             }
             Err(_) => {
